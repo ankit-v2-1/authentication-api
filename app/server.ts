fe652a5b0c5b36f5a3ca0fbcf8a336c3b8e6a4a9
Ankit raj <ankitjaiswal7890@gmail.com>
2022-04-06 23:58:04 +0530

    build: add views directory and routes

--- app/server.ts
+++ app/server.ts
@@ -1,20 +1,22 @@
 import express from "express";
+import path from "path";
 import 'dotenv/config';
 import bodyParser from 'body-parser';
 import router from './api/routes/index';
 import pool from "./config/db.connection";
+import userRoute from './views/views';
 
-
+// create express app.
 const app = express();
 app.use(bodyParser.json());
 
-const PORT = 3000;
-
+// listen for requests at specified port.
 app.listen(process.env.PORT, () => {
     console.log('app listening at http://localhost:', process.env.PORT);
 
 });
 
+// connect to the database
 pool.getConnection((err) => {
     if (err) {
         console.log("Database Connection Failed!", err);
@@ -23,4 +25,19 @@ pool.getConnection((err) => {
     }
 });
 
-app.use(router);
+
+
+app.set('views', path.join(__dirname, 'views'));
+
+// set the view engin to pug
+app.set('view engine', 'pug');
+
+// ADD puclic files like css js etc to the public folder.
+app.use(express.static(path.join(__dirname, './public')));
+
+// API(backend) routes imported from api/routes/index.js.
+app.use('/api', router);
+
+// User(frontend) routes imported from views/views.js.
+app.use('/', userRoute);
+
